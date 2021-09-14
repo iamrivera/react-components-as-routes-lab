d0ffa9c2fb33b3296b2d44a14f5ec65625f312c7
iamrivera <a3m3rivera@gmail.com>
2021-09-13 20:45:15 -0700

    commit tree 66f126d882ff365201c7c88527f02fa2ede7b072

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => {
+        return (
+          <div key={idx}>
+            <h3>{actor.name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {actor.movies.map((movie) => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
